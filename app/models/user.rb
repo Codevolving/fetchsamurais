119a94e0eacbad5359c5f1580bec14d507332fb7
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-03-10 18:35:15 -0300

    Fix rubocop issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'when create new user' do
+  context 'when creating new user' do
     it { is_expected.to validate_presence_of(:name) }
-  end
-
-  context 'when create new user' do
     it { is_expected.to validate_presence_of(:email) }
   end
 end
